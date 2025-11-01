bf9e9e082076d0e82dbd1cf171217835fc30b733
minjung98 <130021582+minjung98@users.noreply.github.com>
2025-11-01 15:20:46 +0900

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,13 +4,16 @@ import torch
 from model.llava import conversation as conversation_lib
 from model.llava.mm_utils import tokenizer_image_token
 from dataset.caption_datasets.LLavaInstruct_vqa_ds import LLaVAInstructDataset
-from dataset.emo_recog_datasets.CAER_S_EmoRecog_ds import CAER_S_EmoRecog_Dataset
-from dataset.exp_recon_datasets.CAER_S_ExpRecon_ds import CAER_S_ExpRecon_Dataset
+from dataset.category_datasets.CAER_S_category_ds import CAER_S_Cate_Dataset
+from dataset.generation_datasets.CAER_S_generation_quote_ds import CAER_S_Quote_Gen_Dataset
+from dataset.generation_datasets.CAER_S_generation_situation_ds import CAER_S_Situation_Gen_Dataset
+from dataset.reasoning_datasets.CAER_S_reasoning_ds import CAER_S_Reason_Dataset
+from dataset.visual_dataset.CAER_S_visual_ds import CAER_S_Visual_Dataset
 from tools.utils import DEFAULT_IMAGE_TOKEN, IGNORE_INDEX, DEFAULT_IM_END_TOKEN, DEFAULT_IM_START_TOKEN
 
 
 class HybridDatasetBase(torch.utils.data.Dataset):
-    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, dataset, datasets_config, epoch_samples=10000, batch_size=2, precision="fp32", validation=False, sample_rate=None):
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, dataset, datasets_config, epoch_samples=10000, batch_size=4, precision="fp32", validation=False, sample_rate=None):
         self.dataset_dir = dataset_dir
         self.tokenizer = tokenizer
         self.clip_image_encoder = clip_image_encoder
@@ -19,35 +22,25 @@ class HybridDatasetBase(torch.utils.data.Dataset):
         self.epoch_samples = epoch_samples
         self.batch_size = batch_size
         self.precision = precision
+        # task당 데이터셋이 여러개 있는 경우
         self.dataset_list = dataset.split("||")
+        # sample_rate이라는 것은 하나의 task 내에서 각 데이터셋의 비율을 정해주는 것(지정 안 하면 그냥 모두 같은 비율로 뽑음)
         self.sample_rate = np.array(sample_rate or [1.0] * len(self.dataset_list))
         self.sample_rate /= self.sample_rate.sum()
         self.validation = validation
         self.all_datasets = self.create_datasets()
-        print(f"validation: {self.validation}")
     
     def create_datasets(self):
         datasets = []
         for ds in self.dataset_list:
             dataset_cls = self.datasets_config.get(ds)
-            print(f"dataset_cls: {dataset_cls}")
             print("ds: ", ds)
             if dataset_cls:
-                if ds == 'Emotion_Recog':
-                    datasets.append(
-                        dataset_cls(
-                            self.dataset_dir, self.tokenizer, self.clip_image_encoder, self.epoch_samples, self.precision, self.validation, self.sample_rate)
-                        )
-                elif ds == 'Expression_Recon':
-                    datasets.append(
-                        dataset_cls(
-                            self.dataset_dir, self.tokenizer, self.clip_image_encoder, self.epoch_samples, self.precision, self.validation, self.sample_rate)
-                        )
-                else:
-                    datasets.append(
-                        dataset_cls(
-                            self.dataset_dir, self.tokenizer, self.clip_image_encoder, self.epoch_samples, self.precision, self.validation, self.sample_rate)
-                        )
+                datasets.append(
+                    dataset_cls(
+                        self.dataset_dir, self.tokenizer, self.clip_image_encoder, self.epoch_samples, self.precision, self.validation, self.sample_rate)
+                    )
+        print(f"datasets: {datasets}")
         return datasets
 
     def __len__(self):
@@ -57,41 +50,56 @@ class HybridDatasetBase(torch.utils.data.Dataset):
         dataset_idx = np.random.choice(len(self.dataset_list), p=self.sample_rate)
         selected_dataset = self.all_datasets[dataset_idx]
         data = selected_dataset[idx]
+        print("idx: ", idx)
         return (*data,)
 
-class HybridRecognitionDataset(HybridDatasetBase):
-    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, epoch_samples=10000, batch_size=2,
-                 precision="fp32",dataset="SFEW_recog_dataset||CAER-S_recog_dataset", validation=False, sample_rate=None):
-        datasets_config = {"SFEW_recog_dataset": SFEW_EmoRecog_Dataset,
-                           "CAER-S_recog_dataset": CAER_S_EmoRecog_Dataset,
-                           }
+class HybridGenDataset(HybridDatasetBase):
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, epoch_samples=10000, batch_size=4,
+                 precision="fp32",dataset="CAER_S_generation_quote_dataset||CAER_S_generation_situation_dataset", validation=False, sample_rate=None):
+        datasets_config = {"CAER_S_generation_quote_dataset": CAER_S_Quote_Gen_Dataset,
+                           "CAER_S_generation_situation_dataset": CAER_S_Situation_Gen_Dataset}
         super().__init__(
             dataset_dir=dataset_dir, tokenizer = tokenizer, clip_image_encoder = clip_image_encoder, 
             dataset=dataset, datasets_config=datasets_config, epoch_samples=epoch_samples, batch_size=batch_size,
             precision=precision, validation=validation, sample_rate=sample_rate)
 
-class HybridReconstructionDataset(HybridDatasetBase):
-    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, epoch_samples=10000, batch_size=2,
-                 precision="fp32", dataset="SFEW_recon_dataset||CAER-S_recon_dataset", validation=False, sample_rate=None):
-        datasets_config = {"SFEW_recon_dataset": SFEW_ExpRecon_Dataset,
-                           "CAER-S_recon_dataset": CAER_S_ExpRecon_Dataset,
+class HybridCateDataset(HybridDatasetBase):
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder, epoch_samples=10000, batch_size=4,
+                 precision="fp32", dataset="CAER_S_category_dataset", validation=False, sample_rate=None):
+        datasets_config = {"CAER_S_category_dataset": CAER_S_Cate_Dataset
                            }
         super().__init__(
             dataset_dir=dataset_dir, tokenizer = tokenizer, clip_image_encoder = clip_image_encoder, 
             dataset=dataset, datasets_config=datasets_config, epoch_samples=epoch_samples, batch_size=batch_size,
             precision=precision, validation=validation, sample_rate=sample_rate)
-
 
 class HybridCapDataset(HybridDatasetBase):
-    def __init__(self, dataset_dir, tokenizer, clip_image_encoder,epoch_samples=10000, batch_size=2,
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder,epoch_samples=10000, batch_size=4,
                  precision="fp32", dataset="llava_instruct_150k", validation=False, sample_rate=None):
         datasets_config = {"llava_instruct_150k": LLaVAInstructDataset}
         super().__init__(
             dataset_dir=dataset_dir, tokenizer = tokenizer, clip_image_encoder = clip_image_encoder, 
             dataset=dataset, datasets_config=datasets_config, epoch_samples=epoch_samples, batch_size=batch_size,
             precision=precision, validation=validation, sample_rate=sample_rate)
+        
+class HybridReasonDataset(HybridDatasetBase):
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder,epoch_samples=10000, batch_size=4,
+                 precision="fp32", dataset="CAER_S_reasoning_dataset", validation=False, sample_rate=None):
+        datasets_config = {"CAER_S_reasoning_dataset": CAER_S_Reason_Dataset}
+        super().__init__(
+            dataset_dir=dataset_dir, tokenizer = tokenizer, clip_image_encoder = clip_image_encoder, 
+            dataset=dataset, datasets_config=datasets_config, epoch_samples=epoch_samples, batch_size=batch_size,
+            precision=precision, validation=validation, sample_rate=sample_rate)
 
-
+class HybridVisualDataset(HybridDatasetBase):
+    def __init__(self, dataset_dir, tokenizer, clip_image_encoder,epoch_samples=10000, batch_size=4,
+                 precision="fp32", dataset="CAER_S_visual_dataset", validation=False, sample_rate=None):
+        datasets_config = {"CAER_S_visual_dataset": CAER_S_Visual_Dataset}
+        super().__init__(
+            dataset_dir=dataset_dir, tokenizer = tokenizer, clip_image_encoder = clip_image_encoder, 
+            dataset=dataset, datasets_config=datasets_config, epoch_samples=epoch_samples, batch_size=batch_size,
+            precision=precision, validation=validation, sample_rate=sample_rate)
+        
 def custom_collate_fn(batch, tokenizer=None, use_mm_start_end=True, inference=False, local_rank=-1):
     # Initializing lists and counters
     image_path_list, clip_enc_image_list = [], []
@@ -105,7 +113,7 @@ def custom_collate_fn(batch, tokenizer=None, use_mm_start_end=True, inference=Fa
         image_path_list.append(image_path)
         clip_enc_image_list.append(clip_enc_image)
         conversation_list.extend(conversations)
-        gt_exps.append(torch.empty(0,50) if exps is None else exps.float())
+        gt_exps.append(torch.empty(0,103) if exps is None else exps.float())
         questions_list.append(questions)
         offset_list.append(cnt := cnt + len(conversations))
         inferences.append(inference)
@@ -141,7 +149,6 @@ def custom_collate_fn(batch, tokenizer=None, use_mm_start_end=True, inference=Fa
     return {
         "image_paths": image_path_list,
         "clip_enc_images": torch.stack(clip_enc_image_list, dim=0),
-        # "clip_enc_images" : clip_enc_image_list,
         "input_ids": input_ids,
         "labels": targets,
         "attention_masks": attention_masks,
